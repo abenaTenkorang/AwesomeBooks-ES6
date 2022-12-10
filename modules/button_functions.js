28737a6c71a6a7c048f16f5696e76c31dbd2d8dc
abenaTenkorang <abenagy16@gmail.com>
2022-12-10 15:55:19 +0000

    Add Button_function.js file to modules folder

--- modules/button_functions.js
+++ modules/button_functions.js
@@ -1,1 +1,20 @@
-﻿
+const ListMenu = document.querySelector('.list');
+const AddMenu = document.querySelector('.add');
+const ContactMenu = document.querySelector('.contactmenu');
+
+ListMenu.addEventListener('click', () => {
+  document.querySelector('.awesome-books').style.display = 'block';
+  document.querySelector('.new-book').style.display = 'none';
+  document.querySelector('.contact').style.display = 'none';
+});
+AddMenu.addEventListener('click', () => {
+  document.querySelector('.awesome-books').style.display = 'none';
+  document.querySelector('.new-book').style.display = 'block';
+  document.querySelector('.contact').style.display = 'none';
+});
+
+ContactMenu.addEventListener('click', () => {
+  document.querySelector('.awesome-books').style.display = 'none';
+  document.querySelector('.new-book').style.display = 'none';
+  document.querySelector('.contact').style.display = 'block';
+});
