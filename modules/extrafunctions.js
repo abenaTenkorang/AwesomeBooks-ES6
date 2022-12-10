273156b91fa4857dab978e4277bb70210790f20c
abenaTenkorang <abenagy16@gmail.com>
2022-12-10 15:56:35 +0000

    Add extrafunctions.js file to modules folder

--- modules/extrafunctions.js
+++ modules/extrafunctions.js
@@ -1,1 +1,38 @@
-﻿
+import {
+  title, author, addBooks, booklist, deleteBook,
+} from './add_remove.js';
+
+import { booksArr } from './onload.js';
+
+// eslint-disable-next-line no-unused-vars
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+
+document.querySelector('.Books-Container').addEventListener('click', (e) => {
+  booksArr.splice(e.target.id, 1);
+  localStorage.setItem('bookArr', JSON.stringify(booksArr));
+  deleteBook(e.target);
+});
+
+const addBtn = document.querySelector('#add-Button');
+const Title = document.querySelector('.title');
+
+addBtn.addEventListener('click', (e) => {
+  if (Title.value === '' || author.value === '') {
+    e.preventDefault();
+  } else {
+    const theBook = {
+      title: title.value,
+      author: author.value,
+    };
+    booksArr.push(theBook);
+    addBooks(booksArr);
+    localStorage.setItem('booksArr', JSON.stringify(booksArr));
+    document.querySelector('.emptymsg').style.display = 'none';
+    booklist.style.border = '1px solid black';
+  }
+});
