f86ee02698b41ac596e0fda9b2f7e13aba9bcb20
abenaTenkorang <abenagy16@gmail.com>
2022-12-10 15:56:56 +0000

    Add onload.js file to modules folder

--- modules/onload.js
+++ modules/onload.js
@@ -1,1 +1,8 @@
-﻿
+// eslint-disable-next-line import/no-mutable-exports
+export let booksArr = [];
+
+export const CheckLocalStorage = () => {
+  if (localStorage.getItem('booksArr')) {
+    booksArr = JSON.parse(localStorage.getItem('booksArr'));
+  }
+};
