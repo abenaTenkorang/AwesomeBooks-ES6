0958ed1031e0c27aee6e796ae8cf53d0846a61bd
abenaTenkorang <abenagy16@gmail.com>
2022-12-10 15:57:24 +0000

    Add display time functionality

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-﻿
+import './modules/button_functions.js';
+import DateTimeFormat from './modules/dateTime.js';
+import { addBooks } from './modules/add_remove.js';
+import { CheckLocalStorage, booksArr } from './modules/onload.js';
+import './modules/extrafunctions.js';
+
+const displayTime = () => setInterval(() => {
+  document.querySelector('#date').innerHTML = DateTimeFormat();
+}, 1000);
+
+// display time
+window.addEventListener('load', displayTime);
+
+window.onload = () => {
+  CheckLocalStorage();
+  addBooks(booksArr);
+};
