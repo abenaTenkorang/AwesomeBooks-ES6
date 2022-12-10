0ed5ad27f0414b7921968362acbcf5c993dec928
abenaTenkorang <abenagy16@gmail.com>
2022-12-10 15:54:51 +0000

    Add add_remove.js file to modules folder

--- modules/add_remove.js
+++ modules/add_remove.js
@@ -1,1 +1,25 @@
-﻿
+export const booklist = document.querySelector('.books-list');
+export const title = document.querySelector('#Title');
+export const author = document.querySelector('#Author');
+
+export const addBooks = (booksArr) => {
+  booklist.innerHTML = '';
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < booksArr.length; i++) {
+    booklist.innerHTML += `
+      <div class="title-author"> 
+        <p class="Title">"${booksArr[i].title}" by  ${booksArr[i].author}</p>
+        <button class="button remove-books" id = "${i}">Remove</button>
+      </div>
+     `;
+    title.value = '';
+    author.value = '';
+    title.focus();
+  }
+};
+
+export const deleteBook = (el) => {
+  if (el.classList.contains('remove-books')) {
+    el.parentElement.remove();
+  }
+};
