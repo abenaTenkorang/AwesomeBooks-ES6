843f63884723f17224ce06948e1fd18e4ec16da1
abenaTenkorang <abenagy16@gmail.com>
2022-12-12 14:53:48 +0000

    Delete button target to remove

--- modules/extrafunctions.js
+++ modules/extrafunctions.js
@@ -1,22 +1,17 @@
 import {
-  title, author, addBooks, booklist, deleteBook,
+  title, author, addBooks, booklist,
 } from './add_remove.js';
 
-import { booksArr } from './onload.js';
+import { booksArr, CheckLocalStorage } from './onload.js';
 
 // eslint-disable-next-line no-unused-vars
 class Book {
-  constructor(title, author) {
+  constructor(title, author, id) {
     this.title = title;
     this.author = author;
+    this.id = id;
   }
 }
-
-document.querySelector('.Books-Container').addEventListener('click', (e) => {
-  booksArr.splice(e.target.id, 1);
-  localStorage.setItem('bookArr', JSON.stringify(booksArr));
-  deleteBook(e.target);
-});
 
 const addBtn = document.querySelector('#add-Button');
 const Title = document.querySelector('.title');
@@ -25,14 +20,17 @@ addBtn.addEventListener('click', (e) => {
   if (Title.value === '' || author.value === '') {
     e.preventDefault();
   } else {
-    const theBook = {
-      title: title.value,
-      author: author.value,
-    };
-    booksArr.push(theBook);
+    CheckLocalStorage();
+    const book = new Book(
+      title.value,
+      author.value,
+      booksArr.length,
+    );
+    booksArr.push(book);
     addBooks(booksArr);
     localStorage.setItem('booksArr', JSON.stringify(booksArr));
     document.querySelector('.emptymsg').style.display = 'none';
     booklist.style.border = '1px solid black';
+    window.location.reload();
   }
 });
