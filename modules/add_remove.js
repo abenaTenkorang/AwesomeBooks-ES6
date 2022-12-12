9bf0bbc867aeb65cd98213c7491f0a71746e2456
abenaTenkorang <abenagy16@gmail.com>
2022-12-12 14:53:22 +0000

    Add new book to already existing books

--- modules/add_remove.js
+++ modules/add_remove.js
@@ -1,20 +1,44 @@
+import { CheckLocalStorage } from './onload.js';
+
 export const booklist = document.querySelector('.books-list');
 export const title = document.querySelector('#Title');
 export const author = document.querySelector('#Author');
 
 export const addBooks = (booksArr) => {
-  booklist.innerHTML = '';
   // eslint-disable-next-line no-plusplus
   for (let i = 0; i < booksArr.length; i++) {
-    booklist.innerHTML += `
-      <div class="title-author"> 
-        <p class="Title">"${booksArr[i].title}" by  ${booksArr[i].author}</p>
-        <button class="button remove-books" id = "${i}">Remove</button>
-      </div>
-     `;
+    const titleAndAuthor = document.createElement('div');
+    titleAndAuthor.classList.add('title-author');
+    booklist.append(titleAndAuthor);
+    const bookDetails = document.createElement('p');
+    bookDetails.classList.add('Title');
+    bookDetails.innerText = `"${booksArr[i].title}" by  ${booksArr[i].author}`;
+    titleAndAuthor.append(bookDetails);
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('button', 'remove-books');
+    removeBtn.id = `${booksArr[i].id}`;
+    removeBtn.innerText = 'Remove';
+    titleAndAuthor.append(removeBtn);
+    const removeBook = () => {
+      const target = removeBtn.id;
+      booksArr.splice(target, 1);
+
+      let refreshId = 0;
+      if (booksArr.length > 0) {
+        booksArr.forEach((e) => {
+          e.id = refreshId;
+          refreshId += 1;
+        });
+      }
+    };
+    removeBtn.addEventListener('click', () => {
+      CheckLocalStorage();
+      removeBook();
+      localStorage.setItem('booksArr', JSON.stringify(booksArr));
+      window.location.reload();
+    });
     title.value = '';
     author.value = '';
-    title.focus();
   }
 };
 
